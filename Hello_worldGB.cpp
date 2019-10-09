6433f18e98d1c8ce68dce6a83c2b44cf7cb1cdaa
Ubuntu <ubuntu@ip-172-31-19-198.ec2.internal>
2019-10-09 21:32:09 +0000

    initial commit

---
+++ Hello_worldGB.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Douglass Jeffrey
+// Created on: oct 2019
+// This program determines if a number is positive negative or 0
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello from AWS Cloud9!\n");
+	return 0;
+}
